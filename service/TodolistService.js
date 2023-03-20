ff73de639971dc4cddd7b68071d3239131055775
trisu <tris.usec@gmail.com>
2023-03-20 14:40:55 +0700

    BE: todolist enhance perfomance query

--- service/TodolistService.js
+++ service/TodolistService.js
@@ -57,6 +57,41 @@ class TodolistService{
       };
 
 
+    // static updateTodolist = async (id, title, category, isNotified, hour, text, isCompleted) => {
+    //     console.log("Update Todolist");
+    //     const user = auth.currentUser;
+    //     let vTimeNotified;
+    //     if(isNotified){ 
+    //         const timeArray = hour.split(":");
+    //         vTimeNotified = timeArray[0] + ":" + timeArray[1];
+    //     }else{
+    //         vTimeNotified = "";
+    //     }
+
+    //     const updatedData = {
+    //         id: id, title: title, 
+    //         category: category, isNotified: isNotified, 
+    //         hour: vTimeNotified,text: text,isCompleted: isCompleted
+    //     }
+
+    //     const userRef = doc(collection(firestore, 'todolist'), user.uid);
+    //     try {
+    //         const userDoc = await getDoc(userRef);
+    //         const todolist = userDoc.data().todolist;
+    //         const updatedTodolist = todolist.map((item) => {
+    //             if (item.id === id) {
+    //             return { ...item, ...updatedData };
+    //             } else {
+    //             return item;
+    //             }
+    //         });
+    //         await updateDoc(userRef, { todolist: updatedTodolist });
+    //         console.log("update OK");
+    //     } catch (error) {
+    //         console.log("error: ", error);
+    //     }
+    // };
+
     static updateTodolist = async (id, title, category, isNotified, hour, text, isCompleted) => {
         console.log("Update Todolist");
         const user = auth.currentUser;
@@ -78,15 +113,16 @@ class TodolistService{
         try {
             const userDoc = await getDoc(userRef);
             const todolist = userDoc.data().todolist;
-            const updatedTodolist = todolist.map((item) => {
-                if (item.id === id) {
-                return { ...item, ...updatedData };
-                } else {
-                return item;
-                }
-            });
-            await updateDoc(userRef, { todolist: updatedTodolist });
-            console.log("update OK");
+            const itemIndex = todolist.findIndex(item => item.id === id);
+            if(itemIndex !== -1){
+                const updatedTodolist = [...todolist];
+                updatedTodolist[itemIndex] = {...updatedTodolist[itemIndex], ...updatedData};
+                await updateDoc(userRef, { todolist: updatedTodolist }, {merge: true});
+                console.log("update OKK");
+            }else{
+                console.log("No todolist item found");
+            }
+            
         } catch (error) {
             console.log("error: ", error);
         }
@@ -100,8 +136,8 @@ class TodolistService{
             const userDoc = await getDoc(userRef);
             const todolist = userDoc.data().todolist;
             const updatedTodolist = todolist.filter(item => item.id !== id);
-            await updateDoc(userRef, { todolist: updatedTodolist });
-            console.log("delete OK");
+            await updateDoc(userRef, { todolist: updatedTodolist }, {merge: true});
+            console.log("delete OKK");
         } catch (error) {
             console.log("error: ", error);
         }
